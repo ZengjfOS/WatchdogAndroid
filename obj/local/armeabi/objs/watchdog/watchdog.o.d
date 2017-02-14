9522c2f9895d955d440d8df41cb5351d895d041d
zorozeng <aplexos@163.com>
2017-02-14 18:28:12 +0800

    init.

---
+++ obj/local/armeabi/objs/watchdog/watchdog.o.d
@@ -0,0 +1,148 @@
+obj/local/armeabi/objs/watchdog/watchdog.o: jni/watchdog.c \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/string.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/strings.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/memory.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/signal.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/limits.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/string.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/cygdrive/f/ndk/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
